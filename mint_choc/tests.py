bbb90dd7b53637f795e6b7863e1cbf3d52bd00f8
RAVEN DELANEY <ravend17@gmail.com>
2020-01-13 14:33:20 -0800

    lab done

---
+++ mint_choc/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+# Create your tests here.
+def test_about_page_status(self):
+	url = reverse('about')
+	response = self.client.get(url)
+	self.assertEqual(response.status_code, 200)
+
+def test_about_templates(self):
+	url = reverse('about')
+	response = self.client.get(url)
+	self.assertTemplateUsed(response, 'about.html'),
+	self.assertTemplateUsed(response, 'base.html')
